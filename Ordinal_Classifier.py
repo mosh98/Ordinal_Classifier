b513e76b0b4f9e42992c85dd31cb42c82315054f
moslehmahamud <mosleh.edu@gmail.com>
2022-04-10 13:43:11 +0200

    Initial commit

---
+++ Ordinal_Classifier.py
@@ -0,0 +1,48 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.base import clone, BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_is_fitted, check_array
+from sklearn.utils.multiclass import check_classification_targets
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+
+class OrdinalClassifier(BaseEstimator, ClassifierMixin):
+    #https://towardsdatascience.com/simple-trick-to-train-an-ordinal-regression-with-any-classifier-6911183d2a3c
+    """
+    A classifier that can be trained on a range of classes.
+    @param classifier: A scikit-learn classifier.
+    """
+    def __init__(self,clf):
+        self.clf = clf
+        self.clfs = {}
+
+
+    def fit(self,X,y):
+            self.uniques_class = np.sort(np.unique(y))
+            if self.uniques_class.shape[0] > 2:
+                for i in range(self.uniques_class.shape[0]-1):
+                    binary_y = (y > self.uniques_class[1]).astype(np.uint8)
+                    clf = clone(self.clf)
+                    clf.fit(X,binary_y)
+                    self.clfs[i] = clf
+
+    def predict(self,X):
+        return np.argmax( self.predict_proba(X), axis=1 )
+
+    def predict_proba(self,X):
+        clfs_predict = {k:self.clfs[k].predict_proba(X) for k in self.clfs}
+
+        predicted = []
+
+        for i,y in enumerate(self.unique_class):
+            if i == 0:
+                # V1 = 1 - Pr(y > V1)
+                predicted.append(1 - clfs_predict[y][:, 1])
+            elif y in clfs_predict:
+                #Vi = Pr(y>Vi-1)- Pr(y > Vi)
+                predicted.append(clfs_predict[y-1][:,1] - clfs_predict[y][:,1])
+            else:
+                predicted.append(clfs_predict[y-1][:,1])
+
+        return np.vstack(predicted).T
+
+
---
+++ readme.md
@@ -0,0 +1,45 @@
+
+## Ordinal Classification/Regression
+
+Turn your favourite classifier into an ordinal classifier.
+
+Ordinal classification packages where every sklearn classifier is supported.
+
+Listen here you dum dum: you need to have more than 2 labels for your features obviously!
+
+The classification is based upon this paper over here:
+
+[1] https://dl.acm.org/doi/10.1007/3-540-44795-4_13
+
+
+```python
+pip install ordinal-classification
+```
+
+wait! i havent deployed to pypi yet, gimme a few days.
+
+
+### How do i use it?
+
+1. import your favourite classifier from sklearn
+2. Insert that clf into the OrdinalClassifier
+3. Call fit and predict on your data
+4. Profit!
+5. Profit!
+6. repeat step 4 and 5 for even more profit!
+
+```python
+from oridnal_classifier import Ordinal_Classifier as OC
+from sklearn import Decision Tree Classifier
+
+clf = OC.OrdinalClassifier(DecisionTreeClassifier())
+
+clf.fit(X_train, y_train)
+  
+predictions = clf.predict(X_test)
+
+# TADA! You just became a regression master! 
+# By exploting an unemployed engineer. Remember that when you sleep 
+```
+
+
